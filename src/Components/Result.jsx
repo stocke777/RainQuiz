c3d1ea5c0098042ad23b03191b070e3cc69694ad
stocke777 <deshwaljaivardhan@gmail.com>
2022-06-04 21:14:34 +0530

    added restart button

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,11 +1,19 @@
 import React, { useContext } from 'react'
 import QuizContext from '../Helpers/Context'
+
 function Result() {
   const {gameState, setGameState, score, setScore} = useContext(QuizContext)
+
+  const restartQuiz = () => {
+    setScore(0)
+    setGameState("menu")
+  }
+
   return (
     <>
     <div>Result</div>
     <h1>{score}</h1>
+    <button onClick={()=>{restartQuiz()}}>Restart</button>
     </>
   )
 }
