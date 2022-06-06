f59e7181382b6ee9597192f4ad2002d3c067214b
stocke777 <deshwaljaivardhan@gmail.com>
2022-06-06 16:40:41 +0530

    added working rain effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {React, useState} from "react";
+import {React, useState, useEffect} from "react";
 import Menu from './Components/Menu';
 import Quiz from './Components/Quiz';
 import Result from './Components/Result';
@@ -9,11 +9,12 @@ import QuizContext from "./Helpers/Context";
 function App() {
   const [gameState, setGameState] = useState("menu")
   const [score, setScore] = useState(0)
+
   return (
     <div className="App">
         <h1>QUIZ</h1>
 
-
+        
         <QuizContext.Provider value = {{gameState, setGameState, score, setScore}}>
           {gameState === "menu" && <Menu/>}
           {gameState === "quiz" && <Quiz/>}
